e83eb3eea0b6f2d49897fc66455aa5a69a37a2da
Pixhell78 <36531780+Pixhell78@users.noreply.github.com>
2020-04-13 20:37:45 +0200

    Base de données V1

---
+++ BDD.sql
@@ -0,0 +1,122 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  ven. 03 mai 2019 à 15:40
+-- Version du serveur :  5.7.24
+-- Version de PHP :  7.2.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `ebayece`
+--
+DROP DATABASE IF EXISTS `ebayece`;
+CREATE DATABASE IF NOT EXISTS `ebayece` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `ebayece`;
+
+
+
+--
+-- Structure de la table `acheteur`
+--
+
+DROP TABLE IF EXISTS `acheteur`;
+CREATE TABLE IF NOT EXISTS `acheteur` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NOM` varchar(255) NOT NULL,
+  `PRENOM` varchar(255) NOT NULL,
+  `MAIL` varchar(255) NOT NULL,
+  `PSEUDO` text NOT NULL,
+  `ADRESSE` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `MAIL` (`MAIL`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `banque`
+--
+
+DROP TABLE IF EXISTS `banque`;
+CREATE TABLE IF NOT EXISTS `banque` (
+  `CARTE` varchar(255) NOT NULL,
+  `NUMERO` varchar(255) NOT NULL,
+  `DATE` varchar(255) NOT NULL,
+  `CODE` int(11) NOT NULL,
+  PRIMARY KEY (`NUMERO`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `item`
+--
+
+DROP TABLE IF EXISTS `objet`;
+CREATE TABLE IF NOT EXISTS `objet` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NOM` varchar(255) NOT NULL,
+  `VENDEUR` int(11) NOT NULL,
+  `CATEGORIE` int(11) UNSIGNED NOT NULL,
+  `DESCRIPTION` text NOT NULL,
+  `PRIX` float UNSIGNED NOT NULL,
+  `PHOTO` text NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `vendeur_id` (`VENDEUR`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `vendeur`
+--
+
+DROP TABLE IF EXISTS `vendeur`;
+CREATE TABLE IF NOT EXISTS `vendeur` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NOM` varchar(255) NOT NULL,
+  `PRENOM` varchar(255) NOT NULL,
+  `MAIL` varchar(255) NOT NULL,
+  `PSEUDO` text NOT NULL,
+  `ADRESSE` varchar(255) NOT NULL,
+  `ADMIN` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `MAIL` (`MAIL`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `vendeur`
+--
+
+INSERT INTO `vendeur` (`ID`, `NOM`, `PRENOM`, `MAIL`, `PSEUDO`, `ADRESSE`, `ADMIN`) VALUES
+(1, 'vdb', 'hugo', 'hugo@ece.fr', 'aaa', 'Adresse1', 1);
+
+
+
+--
+-- Contraintes pour la table `item`
+--
+ALTER TABLE `objet`
+  ADD CONSTRAINT `vendeur_id` FOREIGN KEY (`VENDEUR`) REFERENCES `vendeur` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
