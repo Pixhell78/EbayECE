dd5c54f9f3ba7b38f00a0deda3be2b29750346e0
Pixhell78 <36531780+Pixhell78@users.noreply.github.com>
2020-04-16 11:11:55 +0200

    Add files via upload

--- inscription.php
+++ inscription.php
@@ -13,25 +13,25 @@
   // on teste l'existence de nos variables. On teste également si elles ne sont pas vides
 
 if (isset($_POST["typecompte"])) {  // SI ma_radio A BIEN ÉTÉ POSTÉ
-      if ($_POST["typecompte"] == "1") { // SI ma_radio EST ÉGAL À 1 
+      if ($_POST["typecompte"]  == "1") { // SI ma_radio EST ÉGAL À 1 
            if ((isset($_POST['mail']) && !empty($_POST['mail'])) && (isset($_POST['pseudo']) && !empty($_POST['pseudo']))) {
-                     $nom= $_POST['nom'];
-                     $prenom= $_POST['prenom'];
-                     $mail= $_POST['mail'];
-                     $pseudo= $_POST['pseudo'];
-                     $adresse= $_POST['adresse'];
-                     $ville= $_POST['ville'];
+                     $nom       = $_POST['nom'];
+                     $prenom    = $_POST['prenom'];
+                     $mail      = $_POST['mail'];
+                     $pseudo    = $_POST['pseudo'];
+                     $adresse   = $_POST['adresse'];
+                     $ville     = $_POST['ville'];
                      $codepostal= $_POST['codepostal'];
-                     $pays= $_POST['pays'];
+                     $pays      = $_POST['pays'];
 
   if($db_found){
-    $sql ="INSERT INTO acheteur (`id`, `nom`,`prenom`, `mail`, `pseudo`) VALUES (NULL,'".$nom."', '".$prenom."','$mail', '$pseudo');";
-    $result = mysqli_query($db_handle, $sql);
+    $sql                        ="INSERT INTO acheteur (`id`, `nom`,`prenom`, `mail`, `pseudo`) VALUES (NULL,'".$nom."', '".$prenom."','$mail', '$pseudo');";
+    $result                     = mysqli_query($db_handle, $sql);
 
-    $dernierId = mysqli_insert_id($db_handle);
+    $dernierId                  = mysqli_insert_id($db_handle);
 
-    $sql2 ="INSERT INTO adresse(`id`,`acheteur`,`adresse`, `ville`, `codepostal`, `pays`) VALUES (NULL, '$dernierId', '$adresse','$ville','$codepostal','$pays');";
-    $result2 = mysqli_query($db_handle, $sql2);
+    $sql2                       ="INSERT INTO adresse(`id`,`acheteur`,`adresse`, `ville`, `codepostal`, `pays`) VALUES (NULL, '$dernierId', '$adresse','$ville','$codepostal','$pays');";
+    $result2                    = mysqli_query($db_handle, $sql2);
 
     echo '<script type="text/javascript">window.alert("Inscription  réussi !");</script>';
     }
@@ -46,21 +46,21 @@ if (isset($_POST["typecompte"])) {  // SI ma_radio A BIEN ÉTÉ POSTÉ
       else{
 
         if ((isset($_POST['mail']) && !empty($_POST['mail'])) && (isset($_POST['pseudo']) && !empty($_POST['pseudo']))) {
-      $nom= $_POST['nom'];
-      $prenom= $_POST['prenom'];
-      $mail= $_POST['mail'];
-      $pseudo= $_POST['pseudo'];
-      $adresse= $_POST['adresse'];
-      $ville= $_POST['ville'];
+      $nom       = $_POST['nom'];
+      $prenom    = $_POST['prenom'];
+      $mail      = $_POST['mail'];
+      $pseudo    = $_POST['pseudo'];
+      $adresse   = $_POST['adresse'];
+      $ville     = $_POST['ville'];
       $codepostal= $_POST['codepostal'];
-      $pays= $_POST['pays'];
+      $pays      = $_POST['pays'];
 
       $adresse.=" ".$ville." ".$codepostal." ".$pays;
       if($db_found){
                     $sql ="INSERT INTO vendeur (`id`, `nom`,`prenom`, `mail`, `pseudo`,`adresse`,`admin`) VALUES (NULL,'".$nom."', '".$prenom."','$mail', '$pseudo','$adresse ',0);";
                     $result = mysqli_query($db_handle, $sql);
 
-                    echo '<script type="text/javascript">window.alert("Inscription  réussi !");</script>';
+                    echo '<script type="text/javascript">window.alert("Inscription réussi !");</script>';
                     }
 
      }
@@ -76,6 +76,162 @@ if (isset($_POST["typecompte"])) {  // SI ma_radio A BIEN ÉTÉ POSTÉ
   mysqli_close($db_handle);
   ?>
 
+  <!DOCTYPE html>
+<html>
+<head>
+  <title>Page d'accueil</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet"  href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+  <style type="text/css">
+    #first
+    {
+      margin-top: 180px;
+    }
+
+    #slogan
+    {
+      margin-top: -80px;
+    }
+  </style>
+</head>
+
+<body>
+  <!-- Barre de navigation contenant le logo et le menu burger -->
+  <div class="menu">
+    <img src="logo.svg" id="logo" style="display: block; margin-left: auto; margin-right: auto; margin-top: -15px; height: 130px; width: 130px">
+    <input type="checkbox" class="burger">
+    <nav>
+      <div id = "trait"></div>                      
+      <a class="nav-link" href="connexion.php" style="border-bottom: 2px black solid">Connexion</a></li>
+    </nav>
+  </div>
+  <!-- Division contenant le slogan, superposé au carrousel et qui rest en haut de l'écran lorsque l'on scroll -->
+  <div id="slogan">
+    <h3 style="margin-top: 10px"><center>Négociez votre bonheur</center></h3>
+  </div>
+
+
+  <!-- Formulaire d'inscription -->
+  <section class="container-fluid"> 
+    <div class="container" id="first">     
+        <div id="signupbox" style=" margin-top:50px" class="mainbox col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2">
+             <div class="informationgeneral">
+                <div class="entete">
+                    <div class="titre-information"><center>FORMULAIRE D'INSCRIPTION</center></div>
+                </div>  
+                <div class="contenue" >
+                  <form id="signupform" class="form-horizontal" role="form" action="inscription.php" method="post">
+                    <div id="alert_enregist" style="display:none" class="alert alert-danger">
+                      <p>Erreur</p>
+                      <span></span>
+                    </div>           
+                <!-- RENSEIGNEMENT -->
+                      <div class="form-check form-check-inline">
+                                  <input type="radio" class="form-check-input" id="acheteur" name="typecompte" value="1" checked>
+                                 <label class="form-check-label" for="materialInline1">Acheteur</label>
+                          </div>
+
+                        <div class="form-check form-check-inline">
+                             <input type="radio" class="form-check-input" id="vendeur" name="typecompte" value="2">
+                              <label class="form-check-label" for="materialInline2">Vendeur</label>
+                        </div>
+
+                    <div class="form-group">
+                      <label for="prenom" class="col-md-3 control-label">Prenom</label>
+                      <div class="col-md-9">
+                        <input type="text" class="form-control" id="prenom" name="prenom" placeholder="Prenom" required>
+                      </div>
+                    </div>
+                                
+                    <div class="form-group">
+                        <label for="nom" class="col-md-3 control-label">Nom</label>
+                        <div class="col-md-9">
+                          <input type="text" class="form-control" id="nom" name="nom" placeholder="Nom" required>
+                        </div>
+                    </div>
+                                
+                    <div class="form-group">
+                        <label for="adresse" class="col-md-3 control-label">Adresse</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" id="adresse" name="adresse" placeholder="Adresse" required>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                      <label for="pays" class="col-md-3 control-label">Pays</label>
+                      <div class="col-md-9">
+                        <input type="text" class="form-control" id="pays" name="pays" placeholder="Pays">
+                      </div>
+                    </div>
+                                     
+                    <div class="form-group">
+                      <label for="ville" class="col-md-3 control-label">Ville</label>
+                      <div class="col-md-9">
+                          <input type="text" class="form-control" id="ville" name="ville" placeholder="Ville" required>
+                      </div>
+                    </div>
+                                
+                    <div class="form-group">
+                      <label for="code_postal" class="col-md-3 control-label">Code Postal</label>
+                      <div class="col-md-9">
+                        <input type="text" class="form-control" maxlength="5" id="codepostal" name="codepostal" placeholder="Code postal" required>
+                      </div>
+                    </div>
+
+                    <div class="form-group">
+                      <label for="email" class="col-md-3 control-label">Email</label>
+                        <div class="col-md-9">
+                          <input type="text" class="form-control" id="email" name="mail" placeholder="Adresse email" required>
+                        </div>
+                    </div>
+                                    
+                    <div class="form-group">
+                      <label for="pseudo" class="col-md-3 control-label">Pseudonyme</label>
+                      <div class="col-md-9">
+                        <input type="text" id="form-control" class="form-control" name="pseudo" placeholder="Pseudonyme" required>
+                      </div>                
+                    </div>
+
+                    <div class="form-group" >
+                      <!-- Bouttons d'enregistrements et de soumission -->                                        
+                      <div class="col-md-offset-3 col-md-9" style=" margin-top:20px;">
+                        <button id="btn-signup" type="submit" name="button5" class="btn btn-info col-md-12"><i class="icon-hand-right"></i> &nbsp S'enregistrer</button>
+                        <td colspan="2" align="center">
+                        </div>
+                      </div>
+                    </div> 
+                  </form>            
+                </div>
+              </section>
+            </table>
+
+  <!-- Footer -->
+  <footer class="page-footer">
+    <div id = "footer">
+      <div id = "contact">   
+        <h4> CONTACT: </h4>
+      </div>
+      <div id = "info">
+        <p>37 quai de Grenelle, 75015 Paris, France<br>info.webdynamique@ece.fr</p>
+      </div>
+      <div id = "tel">
+        <p>+33901234567 <br> +33901234561</p>
+      </div>
+      <div id = "follow">
+        <h4>SUIVEZ-NOUS ! FOLLOW-US !<a href="https://www.facebook.com/"><img src="facebook.svg"></a><a href="https://www.instagram.com/"><img src="instagram.svg" style="margin-top: 5px"></a></h4>
+      </div>
+    </div>
+
+  </footer>
+
+</body>
+</html>s
 <!DOCTYPE html>
 <html>
 <head>
@@ -100,7 +256,7 @@ if (isset($_POST["typecompte"])) {  // SI ma_radio A BIEN ÉTÉ POSTÉ
 
   <!-- Deuxième barre de navigation contenant le logo -->
   <nav class="navbar navbar-expand-md" id="logo">
-    <a class="navbar-brand" href="#"><img src="logo.jpg" style="width : 100px; height : 75px;"></a>
+    <a class="navbar-brand" href="#"><img src="logo.svg" style="width : 100px; height : 75px;"></a>
   </nav>
 
 
