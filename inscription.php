4d6720e0d9a1ba769aed30321252c52418169bb6
Pixhell78 <36531780+Pixhell78@users.noreply.github.com>
2020-04-17 14:06:42 +0200

    Add files via upload

--- inscription.php
+++ inscription.php
@@ -98,6 +98,82 @@ if (isset($_POST["typecompte"])) {  // SI ma_radio A BIEN ÉTÉ POSTÉ
     {
       margin-top: -80px;
     }
+
+    body
+    {
+      /*overflow-x: hidden;*/
+    }
+    #info1, #info2, #info3, #info4
+    {
+      display: flex;
+      flex-direction: row;
+      width: 1300px;
+      height: 150px;
+    }
+
+    #type
+    {
+      width: 1800px;
+    }
+
+    #city
+    {
+      margin-left: -269px;
+    }
+
+    #name, #add, #code, #email
+    {
+      margin-left: 100px; 
+    }
+
+    #firstname
+    {
+      margin-left: 200px;
+    }
+
+    #country
+    {
+      margin-left: 200px;
+    }
+
+    #password
+    {
+      margin-left: -265px;
+    }
+
+    #txtMdp, #txtCode, #txtMail
+    {
+      width: 800px;
+    }
+
+    #enregistrer
+    {
+      width: 1300px;
+    }
+
+    input[type = "radio"]
+    {
+      display: none;
+      cursor: pointer;
+    }
+
+    input[type = "radio"] + label::before
+    {
+      content: "";
+      cursor: pointer;
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+      background-color: white;
+      border: 2px black solid;
+      border-radius: 2px;
+      margin-left: 300px; 
+    }
+
+    input[type = "radio"]:checked + label::before
+    {
+      background-color: black;
+    }
   </style>
 </head>
 
@@ -116,95 +192,99 @@ if (isset($_POST["typecompte"])) {  // SI ma_radio A BIEN ÉTÉ POSTÉ
     <h3 style="margin-top: 10px"><center>Négociez votre bonheur</center></h3>
   </div>
 
-
+  <div id="first" style="margin-top: 180px; border: 2px black solid; width: 350px; margin-right: auto; margin-left: auto">
+    <center><h3 style="margin-top: 10px">Formulaire d'inscription</h3></center>
+  </div>
   <!-- Formulaire d'inscription -->
   <section class="container-fluid"> 
-    <div class="container" id="first">     
+    <div class="container">     
         <div id="signupbox" style=" margin-top:50px" class="mainbox col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2">
-             <div class="informationgeneral">
-                <div class="entete">
-                    <div class="titre-information"><center>FORMULAIRE D'INSCRIPTION</center></div>
-                </div>  
+             <div class="informationgeneral"> 
                 <div class="contenue" >
                   <form id="signupform" class="form-horizontal" role="form" action="inscription.php" method="post">
                     <div id="alert_enregist" style="display:none" class="alert alert-danger">
                       <p>Erreur</p>
                       <span></span>
-                    </div>           
-                <!-- RENSEIGNEMENT -->
-                      <div class="form-check form-check-inline">
-                                  <input type="radio" class="form-check-input" id="acheteur" name="typecompte" value="1" checked>
-                                 <label class="form-check-label" for="materialInline1">Acheteur</label>
-                          </div>
+                    </div>
 
+                    <div class="form-check form-check-inline" id="type">
+                          <input type="radio" class="form-check-input" id="acheteur" name="typecompte" value="1" checked>
+                          <label for="acheteur" style="font-size: 20px;">    Acheteur</label><br>
+                        </div>
                         <div class="form-check form-check-inline">
-                             <input type="radio" class="form-check-input" id="vendeur" name="typecompte" value="2">
-                              <label class="form-check-label" for="materialInline2">Vendeur</label>
+                          <input type="radio" class="form-check-input" id="vendeur" name="typecompte" value="2">
+                          <label for="vendeur" style="font-size: 20px;">       Vendeur</label>
                         </div>
+                      </div>          
 
-                    <div class="form-group">
-                      <label for="prenom" class="col-md-3 control-label">Prenom</label>
-                      <div class="col-md-9">
-                        <input type="text" class="form-control" id="prenom" name="prenom" placeholder="Prenom" required>
-                      </div>
-                    </div>
-                                
-                    <div class="form-group">
-                        <label for="nom" class="col-md-3 control-label">Nom</label>
-                        <div class="col-md-9">
-                          <input type="text" class="form-control" id="nom" name="nom" placeholder="Nom" required>
-                        </div>
-                    </div>
-                                
-                    <div class="form-group">
-                        <label for="adresse" class="col-md-3 control-label">Adresse</label>
-                        <div class="col-md-9">
-                            <input type="text" class="form-control" id="adresse" name="adresse" placeholder="Adresse" required>
-                        </div>
-                    </div>
+                      
+              <div id="info1">
+                <div class="form-group" id="name">
+                  <label for="nom" class="col-md-3 control-label"><h4>Nom</h4></label>
+                  <div class="col-md-9">
+                    <input style="border: 2px black solid; width: 300px" type="text" class="form-control" id="nom" name="nom" placeholder="Exemple: Chamberlain" required>
+                  </div>
+                </div>
+                <div class="form-group" id="firstname">
+                  <label for="prenom" class="col-md-3 control-label"><h4>Prénom</h4></label>
+                  <div class="col-md-9">
+                    <input style="border: 2px black solid; width: 300px" type="text" class="form-control" id="prenom" name="prenom" placeholder="Exemple: Wilt" required>
+                  </div>
+                </div>
+              </div>
+            
+            <div id="info2">
+              <div class="form-group" id="add">
+                <label for="adresse" class="col-md-3 control-label"><h4>Adresse</h4></label>
+                <div class="col-md-9">
+                  <input style="border: 2px black solid; width: 300px" type="text" class="form-control" id="adresse" name="adresse" placeholder="Exemple: 13bis rue Julien Chaillioux" required>
+                </div>
+              </div>
 
-                    <div class="form-group">
-                      <label for="pays" class="col-md-3 control-label">Pays</label>
-                      <div class="col-md-9">
-                        <input type="text" class="form-control" id="pays" name="pays" placeholder="Pays">
-                      </div>
-                    </div>
+              <div class="form-group" id="country">
+                <label for="pays" class="col-md-3 control-label"><h4>Pays</h4></label>
+                <div class="col-md-9">
+                  <input type="text" class="form-control" style="border: 2px black solid; width: 300px" id="pays" name="pays" placeholder="Exemple: Zanzibar" required>
+                </div>
+              </div>
+            </div>
+
+
+            <div id="info3">
+              <div class="form-group" id="code">
+                <label for="postal" class="col-md-3 control-label" id="txtCode"><h4>Code Postal</h4></label>
+                <div class="col-md-9">
+                  <input type="text" class="form-control" style="border: 2px black solid; width: 300px" id="codepostal" name="codepostal" placeholder="Exemple: 94260" required>
+                </div>
+              </div>
+
+              <div class="form-group" id="city">
+                <label for="ville" class="col-md-3 control-label" id="txtPrix"><h4 style="text-align: left">Ville</h4></label>
+                <div class="col-md-9">
+                  <input style="width: 300px; border: 2px black solid" type="text" class="form-control" id="ville" name="ville" placeholder="Exemple : Fresnes" required>
+                </div>
+              </div>
+            </div>
                                      
-                    <div class="form-group">
-                      <label for="ville" class="col-md-3 control-label">Ville</label>
-                      <div class="col-md-9">
-                          <input type="text" class="form-control" id="ville" name="ville" placeholder="Ville" required>
-                      </div>
-                    </div>
-                                
-                    <div class="form-group">
-                      <label for="code_postal" class="col-md-3 control-label">Code Postal</label>
-                      <div class="col-md-9">
-                        <input type="text" class="form-control" maxlength="5" id="codepostal" name="codepostal" placeholder="Code postal" required>
-                      </div>
-                    </div>
+            <div id="info4">
+              <div class="form-group" id="email">
+                <label for="mail" class="col-md-3 control-label" id="txtMail"><h4>E-Mail</h4></label>
+                <div class="col-md-9">
+                  <input type="text" class="form-control" style="border: 2px black solid; width: 300px" id="mail" name="mail" placeholder="Exemple: wilt.chamberlain@edu.ece.fr" required>
+                </div>
+              </div>
 
-                    <div class="form-group">
-                      <label for="email" class="col-md-3 control-label">Email</label>
-                        <div class="col-md-9">
-                          <input type="text" class="form-control" id="email" name="mail" placeholder="Adresse email" required>
-                        </div>
-                    </div>
-                                    
-                    <div class="form-group">
-                      <label for="pseudo" class="col-md-3 control-label">Pseudonyme</label>
-                      <div class="col-md-9">
-                        <input type="text" id="form-control" class="form-control" name="pseudo" placeholder="Pseudonyme" required>
-                      </div>                
-                    </div>
+              <div class="form-group" id="password">
+                <label for="mdp" class="col-md-3 control-label" id="txtMdp"><h4>Pseudonyme</h4></label>
+                <div class="col-md-9">
+                  <input type="text" class="form-control" style="border: 2px black solid; width: 300px" id="form-control" name="pseudo" required>
+                </div>
+              </div>
+            </div>
 
-                    <div class="form-group" >
-                      <!-- Bouttons d'enregistrements et de soumission -->                                        
-                      <div class="col-md-offset-3 col-md-9" style=" margin-top:20px;">
-                        <button id="btn-signup" type="submit" name="button5" class="btn btn-info col-md-12"><i class="icon-hand-right"></i> &nbsp S'enregistrer</button>
-                        <td colspan="2" align="center">
-                        </div>
-                      </div>
+                    <div class="form-group" id="enregistrer">
+              <input type="submit" id="envoi" name="button5" value="Envoyer" style="width: 300px; margin-left: 400px; background-color: black; border: 2px black solid;font-family: 'Roboto-bold', sans-serif;" class="btn btn-info col-md-12">     
+            </div>
                     </div> 
                   </form>            
                 </div>
@@ -229,155 +309,5 @@ if (isset($_POST["typecompte"])) {  // SI ma_radio A BIEN ÉTÉ POSTÉ
     </div>
 
   </footer>
-
-</body>
-</html>s
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Page d'accueil</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet"  href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-</head>
-
-<body>
-  <!-- Première barre de navigation contenant un lien vers la page de connexion -->
-  <nav class="navbar navbar-expand-md" id="liens">
-
-    <div class="collapse navbar-collapse" id="main-navigation">      
-      <ul class="navbar-nav">             
-        <li class="nav-item"><a class="nav-link" href="connexion.php">Déjà membre ?</a></li>
-      </ul>                    
-  </nav>
-
-  <!-- Deuxième barre de navigation contenant le logo -->
-  <nav class="navbar navbar-expand-md" id="logo">
-    <a class="navbar-brand" href="#"><img src="logo.svg" style="width : 100px; height : 75px;"></a>
-  </nav>
-
-
-  <!-- Formulaire d'inscription -->
-  <section class="container-fluid footer"> 
-    <div class="container">     
-        <div id="signupbox" style=" margin-top:50px" class="mainbox col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2">
-             <div class="informationgeneral">
-                <div class="entete">
-                    <div class="titre-information"><center>FORMULAIRE D'INSCRIPTION</center></div>
-                </div>  
-                <div class="contenue" >
-                  <form id="signupform" class="form-horizontal" role="form" action="inscription.php" method="post">
-                    <div id="alert_enregist" style="display:none" class="alert alert-danger">
-                      <p>Erreur</p>
-                      <span></span>
-                    </div>           
-                <!-- RENSEIGNEMENT -->
-                    <!-- Material inline 1 -->
-                            <div class="form-check form-check-inline">
-                                  <input type="radio" class="form-check-input" id="acheteur" name="typecompte" value="1" checked>
-                                 <label class="form-check-label" for="materialInline1">Acheteur</label>
-                          </div>
-
-<!-- Material inline 2 -->
-                        <div class="form-check form-check-inline">
-                             <input type="radio" class="form-check-input" id="vendeur" name="typecompte" value="2">
-                              <label class="form-check-label" for="materialInline2">Vendeur</label>
-                        </div>
-
-                    <div class="form-group">
-                      <label for="prenom" class="col-md-3 control-label">Prenom</label>
-                      <div class="col-md-9">
-                        <input type="text" class="form-control" id="prenom" name="prenom" placeholder="Prenom" required>
-                      </div>
-                    </div>
-                                
-                    <div class="form-group">
-                        <label for="nom" class="col-md-3 control-label">Nom</label>
-                        <div class="col-md-9">
-                          <input type="text" class="form-control" id="nom" name="nom" placeholder="Nom" required>
-                        </div>
-                    </div>
-                                
-                    <div class="form-group">
-                        <label for="adresse" class="col-md-3 control-label">Adresse</label>
-                        <div class="col-md-9">
-                            <input type="text" class="form-control" id="adresse" name="adresse" placeholder="Adresse" required>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                      <label for="pays" class="col-md-3 control-label">Pays</label>
-                      <div class="col-md-9">
-                        <input type="text" class="form-control" id="pays" name="pays" placeholder="Pays">
-                      </div>
-                    </div>
-                                     
-                    <div class="form-group">
-                      <label for="ville" class="col-md-3 control-label">Ville</label>
-                      <div class="col-md-9">
-                          <input type="text" class="form-control" id="ville" name="ville" placeholder="Ville" required>
-                      </div>
-                    </div>
-                                
-                    <div class="form-group">
-                      <label for="code_postal" class="col-md-3 control-label">Code Postal</label>
-                      <div class="col-md-9">
-                        <input type="text" class="form-control" maxlength="5" id="codepostal" name="codepostal" placeholder="Code postal" required>
-                      </div>
-                    </div>
-
-                    <div class="form-group">
-                      <label for="email" class="col-md-3 control-label">Email</label>
-                        <div class="col-md-9">
-                          <input type="text" class="form-control" id="email" name="mail" placeholder="Adresse email" required>
-                        </div>
-                    </div>
-                                    
-                    <div class="form-group">
-                      <label for="pseudo" class="col-md-3 control-label">Pseudonyme</label>
-                      <div class="col-md-9">
-                        <input type="text" id="form-control" class="form-control" name="pseudo" placeholder="Pseudonyme" required>
-                      </div>                
-                    </div>
-
-                    <div class="form-group" >
-                      <!-- Bouttons d'enregistrements et de soumission -->                                        
-                      <div class="col-md-offset-3 col-md-9" style=" margin-top:20px;">
-                        <button id="btn-signup" type="submit" name="button5" class="btn btn-info col-md-12"><i class="icon-hand-right"></i> &nbsp S'enregistrer</button>
-                        <td colspan="2" align="center">
-                        </div>
-                      </div>
-                    </div> 
-                  </form>            
-                </div>
-              </section>
-            </table>
-
-  <!-- Footer -->
-  <footer class="page-footer">   
-      <div class="container">    
-        <div class="row">       
-            <div class="col-lg-8 col-md-8 col-sm-12">       
-              <h6 class="text-uppercase font-weight-bold">Information additionnelle</h6>       
-              <p><right>Contactez-nous ! Nous sommes présent sur <a href="https://www.facebook.com/"> Facebook</a> 
-                ou <a href="https://www.instagram.com">Instagram</a></right> !</p>
-                <p><right>Contact us ! We are on <a href="https://www.facebook.com"> Facebook</a> 
-                or <a href="https://www.instagram.com/">Instagram</a></right> !</p>
-                <p> <right><a href="https://www.facebook.com/"><img src="img/facebook.png"style="width:3%"></a>  
-                  or/and<a href="https://www.instagram.com/"><img src="img/instagram.png" style="width:3%"></a></right></p>
-              </div>   
-              <div class="col-lg-4 col-md-4 col-sm-12">       
-                  <h6 class="text-uppercase font-weight-bold">Contact</h6>       
-                  <p>             37, quai de Grenelle, 75015 Paris, France <br>             info@webDynamique.ece.fr <br>             +33 01 02 03 04 05 <br>             +33 01 03 02 05 04       </p>     
-              </div>   
-            </div>   
-            <div class="footer-copyright text-center">&copy; 2020 Copyright | Droit d'auteur: BENARD Antoine, VANDENBOSSCHE Hugo et SAIDANI Sofiane
-          </div>
-        </div>
-    </footer>
-
 </body>
 </html>
